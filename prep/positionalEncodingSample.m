ffaeb63f21d8efc738798e5b868dc5b0a6698a6b
KentaItakura <shikorski42@gmail.com>
2023-07-22 21:57:32 +0900

    [Update] Add comments

--- prep/positionalEncodingSample.m
+++ prep/positionalEncodingSample.m
@@ -1,12 +1,12 @@
+% positional encoding in NeRF
+% This function is performed in the following paper. 
+% NeRF: Representing Scenes as Neural Radiance Fields for View Synthesis
+
 clear;clc;close all
-
+% parameter
 L = 10;
+% xyz
 p1 = [1 2 3]';
-
-
-% freq_bands = reshape(repelem(2.^(0:(L-1)),1,size(p,1)),size(p,1),1,[]);
-% emb_period = [sin(freq_bands.*repmat(p,[1,1,L]));cos(freq_bands.*repmat(p,[1,1,L]))]; %n-60
-% emb_period = reshape(permute(stripdims(emb_period),[1,3,2]),[],size(p,2))';
 
 freq_bands = reshape(repelem(2.^(0:(L-1)),1,size(p1,1)),size(p1,1),1,[]);
 emb_period = [sin(freq_bands.*repmat(p1,[1,1,L]));cos(freq_bands.*repmat(p1,[1,1,L]))];
@@ -17,5 +17,5 @@ freqBands = p1 .* repmat(2.^[0:L-1], [3, 1]);
 sin1 = sin(freqBands);
 % cos=>x, y, z *10 = 30
 cos1 = cos(freqBands);
-
+% combine both sin and cos
 embed = [sin1(:); cos1(:)]
